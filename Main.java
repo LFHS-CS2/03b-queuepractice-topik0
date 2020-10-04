d17c0dc241cad9bf3fc773e4042b1018da7d0c34
Topik <topik@topik.tech>
2020-10-04 15:05:53 -0500

    Wrote the reorder method

--- Main.java
+++ Main.java
@@ -1,19 +1,31 @@
-import java.io.*;
 import java.util.*;
-class Main {
-    public static void main(String[] args)  {
-       TestMain test = new TestMain();
-       test.main(null);
-    }
-    
-    /**
-     * See Readme.md
-     * IMPORTANT: You may use only a single Stack as auxiliary storage. 
-     */
-    public static void reorder(Queue<Integer> q) 
-    {
-        
-    }
+/**
+ * @author Daniel
+ */
 
+class Main {
+	public static void main(String[] args) {
+		TestMain.main(null);
+	}
+
+	/**
+	 * Reorders a queue of integers
+	 */
+	public static void reorder(Queue<Integer> q) {
+		Stack<Integer> stk = new Stack<Integer>();
+		int size = 0;
+		for (; !q.isEmpty(); size++)
+			stk.push(q.poll());
+		while (!stk.isEmpty())
+			q.add(stk.pop());
+		for (int i = 0; i < size; i++) {
+			if (q.peek() > -1)
+				stk.push(q.poll());
+			else
+				q.add(q.poll());
+		}
+		while (stk.size() > 0)
+			q.add(stk.pop());
+	}
 
 }
